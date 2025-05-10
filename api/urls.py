282d2f2bfdee30e8e950309e2562bc991c5d6690
Кирилл Катранов <katranovrpk@gmail.com>
2025-05-10 21:35:40 +0300

    Create urls.py in app Api

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from api.views import send_verification_code
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/email/', send_verification_code),
+    #path('api/v1/auth/token/', EmailSending.as_view()),
+]
--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -25,9 +25,6 @@ from api.views import send_verification_code
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')),
     path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/auth/email/', send_verification_code),
-    #path('api/v1/auth/token/', EmailSending.as_view()),
 ]
